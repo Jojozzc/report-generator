f55b6081c49406997afed2bbc1759e8a5de04f3e
Jojo22c <zzc185@foxmail.com>
2023-01-02 19:22:07 +0800

    refactor

---
+++ RawData.py
@@ -0,0 +1,46 @@
+class RawData:
+    complete_date: str = None
+
+    # 检件编号
+    sample_no: str = None
+
+    # 焊口编号
+    kind_no: str = None
+
+    # 材质
+    material: str = None
+
+    # 规格
+    specification: str = None
+
+    # 底片规格
+    sample_specification: str = None
+
+    # 底片数量
+    sample_cnt: int = None
+
+    # 合格数量
+    qualified_sample_cnt: int = None
+
+    # 合格级别
+    quality_level = None
+
+    def __init__(self,
+                 complete_date: str,
+                 sample_no: str,
+                 kind_no: str,
+                 material: str,
+                 specification: str,
+                 sample_specification: str,
+                 sample_cnt: int,
+                 qualified_sample_cnt: int,
+                 quality_level):
+        self.complete_date = complete_date
+        self.sample_no = sample_no
+        self.kind_no = kind_no
+        self.material = material
+        self.specification = specification
+        self.sample_specification = sample_specification
+        self.sample_cnt = sample_cnt
+        self.qualified_sample_cnt = qualified_sample_cnt
+        self.quality_level = quality_level
---
+++ write_processor.py
@@ -0,0 +1,107 @@
+import docx
+from docx.enum.table import WD_TABLE_ALIGNMENT
+import pandas
+import os
+from RawData import RawData
+
+
+def write_cell(cell, text: str):
+    cell.text = text
+    cell.paragraphs[0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+
+def process(template_path: str, input_file_path: str, title1: str, title2: str, company_name: str, customer: str,
+          method: str, standard: str, target_dir: str):
+
+    base_start = 5
+    raw_data_map = read_raw_data(input_file_path)
+
+    for key in raw_data_map.keys():
+        values = raw_data_map[key]
+        order_id = key
+        quality_level = values[0].quality_level
+        complete_date = values[0].complete_date
+
+        doc = docx.Document(template_path)
+
+        target_path = os.path.join(target_dir, f'{order_id}.docx')
+
+        doc.paragraphs[1].text = doc.paragraphs[1].text + title1
+        doc.paragraphs[2].text = doc.paragraphs[2].text + title2
+        table = doc.tables[0]
+
+        write_cell(table.cell(0, 2), company_name)
+        write_cell(table.cell(0, 11), order_id)
+
+        write_cell(table.cell(1, 2), customer)
+        write_cell(table.cell(1, 11), complete_date)
+
+        write_cell(table.cell(2, 2), method)
+        write_cell(table.cell(2, 8), standard)
+
+        write_cell(table.cell(2, 14), format(quality_level, '.0%'))
+
+        kind_count = 0
+        unqualified_kind_cnt = 0
+
+        sample_cnt = 0
+        unqualified_sample_cnt = 0
+
+        # 数据从第5行开始写入
+        for i in range(len(values)):
+            raw_data: RawData = values[i]
+
+            # 检件编号
+            write_cell(table.cell(i + base_start, 1), raw_data.sample_no)
+
+            # 焊口编号
+            write_cell(table.cell(i + base_start, 3), raw_data.kind_no)
+
+            # 材质
+            write_cell(table.cell(i + base_start, 5), raw_data.material)
+
+            # 规格
+            write_cell(table.cell(i + base_start, 7), raw_data.specification)
+
+            # 底片规格、数量
+            sample_cnt = sample_cnt + raw_data.sample_cnt
+            write_cell(table.cell(i + base_start, 9), f'{raw_data.sample_specification}/{raw_data.sample_cnt}张')
+
+            # 检测结果(合格)
+            write_cell(table.cell(i + base_start, 13), f'{raw_data.qualified_sample_cnt}张')
+
+            # 检测结果(不合格)
+            unqualified_cnt = raw_data.sample_cnt - raw_data.qualified_sample_cnt
+            if unqualified_cnt > 0:
+                unqualified_kind_cnt = unqualified_kind_cnt + 1
+                write_cell(table.cell(i + base_start, 15), f'{unqualified_cnt}张')
+            else:
+                write_cell(table.cell(i + base_start, 15), '/')
+
+            unqualified_sample_cnt = unqualified_sample_cnt + unqualified_cnt
+            kind_count = kind_count + 1
+
+        if kind_count < 18:
+            write_cell(table.cell(kind_count + base_start, 1), '以下空白')
+
+        table.cell(18 + base_start,
+                   1).text = f'说明：共检测焊口{kind_count}道口，总计{sample_cnt}张底片。其中不合格焊{unqualified_kind_cnt}道，不合格底片{unqualified_sample_cnt}张。'
+
+        doc.save(target_path)
+
+
+def read_raw_data(input_file_path: str):
+    raw_datas = pandas.read_excel(input_file_path, 'Sheet1')
+    data_map = {}
+
+    for i in range(raw_datas.shape[0]):
+        row = raw_datas.iloc[i]
+        order_id = row[2]
+        if order_id not in data_map:
+            data_map[order_id] = []
+
+        raw_data = RawData(str(row[1]), str(row[3]), str(row[4]), str(row[7]), str(row[6]), str(row[10]), row[11],
+                           row[12], row[8])
+        data_map[order_id].append(raw_data)
+
+    return data_map
