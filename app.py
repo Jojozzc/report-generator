236937fd6b3f5d96ece6aa2e4f623d77402e3c6b
Jojo <zzc185@foxmail.com>
2023-06-21 15:48:04 +0800

    ray

--- app.py
+++ app.py
@@ -97,41 +97,66 @@ def build_ray_config():
         "material": ExcelFiledProperty('H', cast_util.wrap_str, '材质'),
         "level": ExcelFiledProperty('I', cast_util.wrap_str, '合格级别'),
         "checkRatioKind": ExcelFiledProperty('J', cast_util.wrap_str, '检测比列'),
+        "isOk": ExcelFiledProperty('K', cast_util.wrap_str, '返修补片'),
         "baseSpecificationAndCnt": ExcelFiledProperty('L', cast_util.wrap_str, '底片规格/张'),
-        "unitName": ExcelFiledProperty('Q', cast_util.wrap_str, '单元名称'),
         "checkCount": ExcelFiledProperty('M', cast_util.wrap_int, '张数'),
         "okCount": ExcelFiledProperty('N', cast_util.wrap_int, '合格数量'),
         "ray": ExcelFiledProperty('P', cast_util.wrap_int, 'γ射线'),
+        "unitName": ExcelFiledProperty('Q', cast_util.wrap_str, '单元名称'),
+        "weldMethod": ExcelFiledProperty('R', cast_util.wrap_str, '焊接方法'),
+        "lineNo": ExcelFiledProperty('R', cast_util.wrap_str, '单线号'),
+
     }
+
+    
     column_cell_resource_list = [
-        MappingColumnCellsResource(table_index=0, table_data_start_row=5, column_view_index=0,
+        MappingColumnCellsResource(table_index=0, table_data_start_row=7, column_view_index=1,
                                    mapping_data_key='sampleNo'),
-        MappingColumnCellsResource(table_index=0, table_data_start_row=5, column_view_index=2,
+        MappingColumnCellsResource(table_index=0, table_data_start_row=7, column_view_index=2,
+                                   mapping_data_key='lineNo'),
+        MappingColumnCellsResource(table_index=0, table_data_start_row=7, column_view_index=3,
                                    mapping_data_key='kindNo'),
-        MappingColumnCellsResource(table_index=0, table_data_start_row=5, column_view_index=4,
-                                   mapping_data_key='material'),
-        MappingColumnCellsResource(table_index=0, table_data_start_row=5, column_view_index=5,
+        MappingColumnCellsResource(table_index=0, table_data_start_row=7, column_view_index=4,
+                                   mapping_data_key='empId'),
+        MappingColumnCellsResource(table_index=0, table_data_start_row=7, column_view_index=5,
                                    mapping_data_key='specification'),
-        MappingColumnCellsResource(table_index=0, table_data_start_row=5, column_view_index=9,
-                                   mapping_data_key='baseSpecificationAndCnt'),
-        MappingColumnCellsResource(table_index=0, table_data_start_row=5, column_view_index=12,
-                                   mapping_data_key='okCount'),
-        SimpleCalculationColumnCellsResource(table_index=0, table_data_start_row=5, column_view_index=14,
-                                             mapping_data_key_1='checkCount', mapping_data_key_2='okCount',
-                                             operation='-'),
+        MappingColumnCellsResource(table_index=0, table_data_start_row=7, column_view_index=6,
+                                   mapping_data_key='material'),
     ]
 
     doc_global_data_param_config_list = [
-        DocGlobalParamConfig('projectName', None, '工程名称', False),
+        DocGlobalParamConfig('customorCompany', None, '委托单位', False),
         DocGlobalParamConfig('testingStandards', None, '检测标准', False),
+        DocGlobalParamConfig('detectionOpportunity', None, '检测时机', False),
+        DocGlobalParamConfig('acceptStandard', None, '验收规范', False),
+        DocGlobalParamConfig('detectionTechLevel', None, '检测技术等级', False),
+        DocGlobalParamConfig('appearanceDetection', '合格', '外观检查', False),
     ]
 
     cell_resource_list = [
-        DataListMappingCellResource(table_index=0, row=0, column_view_index=14, mapping_key=divide_key),
-        DataListMappingCellResource(table_index=0, row=1, column_view_index=14, mapping_key='completeDate'),
-        DataListMappingCellResource(table_index=0, row=2, column_view_index=14, mapping_key='level'),
+         # 委托单位
+        GlobalParamMappingCellResource(table_index=0, row=1, column_view_index=1, mapping_key='customorCompany'),
+        # 委托单编号
+        DataListMappingCellResource(table_index=0, row=1, column_view_index=3, mapping_key=orderId),
+        # 验收标准
+        GlobalParamMappingCellResource(table_index=0, row=2, column_view_index=5, mapping_key='acceptStandard'),
+        # 检测标准
+        GlobalParamMappingCellResource(table_index=0, row=3, column_view_index=3, mapping_key='testingStandards'),
+        # 检测时机
+        GlobalParamMappingCellResource(table_index=0, row=3, column_view_index=5, mapping_key='detectionOpportunity'),
+        # 检测技术等级
+        GlobalParamMappingCellResource(table_index=0, row=4, column_view_index=1, mapping_key='detectionTechLevel'),
+        # 检测比列
+        DataListMappingCellResource(table_index=0, row=4, column_view_index=3, mapping_key='checkRatioKind'),
+        # 合格级别
+        DataListMappingCellResource(table_index=0, row=4, column_view_index=5, mapping_key='level'),
+        # 焊接方法
+        DataListMappingCellResource(table_index=0, row=5, column_view_index=1, mapping_key='weldMethod'),
+        # 外观检查
+        GlobalParamMappingCellResource(table_index=0, row=5, column_view_index=3, mapping_key='appearanceDetection'),
+
     ]
-    template_path = os.path.join(os.path.dirname(__file__), 'template/rt/TEMPLATE.docx')
+    template_path = os.path.join(os.path.dirname(__file__), 'template/ray/TEMPLATE.docx')
     report_generator = SimpleTableDocGenerator(template_path=template_path, filed_mapping=filed_mapping,
                                                divide_key=divide_key,
                                                header_resource=RTHeaderResource(),
