44cfc6266b02ea1e0910003363a23b5c05c39757
Jojo22c <zzc185@foxmail.com>
2023-07-19 23:32:37 +0800

    support data list sort

--- awe_report_generator/core/util/date_util.py
+++ awe_report_generator/core/util/date_util.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 def get_YYYYmmdd_cn(date_str_by_dot: str):
     """
 
@@ -7,8 +8,20 @@ def get_YYYYmmdd_cn(date_str_by_dot: str):
     :return:
     """
     try:
-        return datetime.datetime.strptime(date_str_by_dot, '%Y.%m.%d').strftime('%Y年%m月%d日'.encode('unicode_escape').decode('utf8')).encode('utf-8').decode('unicode_escape')
+        return datetime.datetime.strptime(date_str_by_dot, '%Y.%m.%d').strftime(
+            '%Y年%m月%d日'.encode('unicode_escape').decode('utf8')).encode('utf-8').decode('unicode_escape')
     except BaseException as e:
         print(f'时间有误:{date_str_by_dot}')
         print(e)
         return None
+
+
+def parse_dot_date_time(date_str_by_dot: str):
+    if date_str_by_dot is None:
+        return datetime.datetime.strptime('1.1.1', '%Y.%m.%d')
+    try:
+        return datetime.datetime.strptime(date_str_by_dot, '%Y.%m.%d')
+    except BaseException as e:
+        print(f'时间有误:{date_str_by_dot}')
+        print(e)
+        return datetime.datetime.strptime('1.1.1', '%Y.%m.%d')
