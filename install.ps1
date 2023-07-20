55fb767ca4b48925974640d0cf5019a919618f33
Jojo <zzc185@foxmail.com>
2023-07-20 10:14:42 +0800

    Update install.ps1
    --noconsole

--- install.ps1
+++ install.ps1
@@ -8,7 +8,7 @@ If(!(test-path -PathType container $templatePath))
 {
       New-Item -ItemType Directory -Path $templatePath
 }
-pyinstaller -F app.py
+pyinstaller -F --noconsole app.py
 Copy-Item "./dist/app.exe" -Destination $path
 Copy-Item -Path "./template" -Destination $templatePath -Recurse
 
